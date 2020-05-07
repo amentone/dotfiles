c414309aad80e6bb1ba0e8ab093d5d5695b215e6
anmentone <mentos@pop-os>
2020-05-07 13:57:26 +0200

    hyper configuration added

---
+++ hyper/import.sh
@@ -0,0 +1,5 @@
+hyper i hyper-search 
+hyper i hyper-highlight-active-pane
+hyper i hyper-fading-scrollbar
+hyper i hyper-opacity
+ln -s -f -v /home/mentos/github/dotfiles/hyper/hyper.js.link /home/mentos/.hyper.js
